6929c2078dbc547fd51fe76336d6a2d4f2fe4b2b
techycs18 <techycs7299@gmail.com>
2020-07-22 17:36:55 +0530

    Human ACtivity Recognition

--- recognise_human_activity.py
+++ recognise_human_activity.py
@@ -11,7 +11,7 @@ class Parameters:
                             ).read().strip().split("\n")
         self.ACTION_RESNET = 'model/resnet-34_kinetics.onnx'
 #         self.VIDEO_PATH = None
-        self.VIDEO_PATH = "test/video1.mp4"
+        self.VIDEO_PATH = "test/example1.mp4"
         # SAMPLE_DURATION is maximum deque size
         self.SAMPLE_DURATION = 16
         self.SAMPLE_SIZE = 112
